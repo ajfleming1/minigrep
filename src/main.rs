9aa8ef239c06ea57f9fa37845380016871b1b960
Drew Fleming <andrewfleming@gmail.com>
2020-09-21 16:39:51 -0400

    Makes things better.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
+extern crate minigrep;
+
+use minigrep::run;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    if let Err(e) = run(config) {
+        eprintln!("Application error! {}", e);
 
-    let mut f = File::open(filename).expect("File not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-    .expect("something went wrong reading the file");
-
-    println!("With text {}", contents);
-}
-
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
+        process::exit(1);
+    }
 }
